ff7327b5f637c85bf88a691509c1795f03aab0bd
Dionisio.277094 <Dionisio.277094@LAB-C-PC10>
2023-10-23 17:49:58 +0800

    Add project files.

---
+++ Thread Priority/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thread_Priority
+{
+    public partial class frmTrack : Form
+    {
+        public frmTrack()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTrackThread_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            Thread ThreadB = new Thread(MyThreadClass.Thread2);
+            Thread ThreadC = new Thread(MyThreadClass.Thread1);
+            Thread ThreadD = new Thread(MyThreadClass.Thread2);
+           
+           
+
+            ThreadA.Name = "ThreadA Process";
+            ThreadB.Name = "ThreadB Process";
+            ThreadC.Name = "ThreadC Process";
+            ThreadD.Name = "ThreadD Process";
+
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+          
+            label1.Text = "-End of Thread";
+            
+;
+        }
+    }
+}
